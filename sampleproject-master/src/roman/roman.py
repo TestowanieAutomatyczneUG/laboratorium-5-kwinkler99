35964cdd1ed0d091ff35895e0c1479426dfe9e8e
Katarzyna Winkler <kasia.winkler99@gmail.com>
2020-11-02 21:42:59 +0100

    02 - test_20_is_two_x_s program

--- sampleproject-master/src/roman/roman.py
+++ sampleproject-master/src/roman/roman.py
@@ -1,9 +1,11 @@
 def roman(arg):
     if arg <= 3:
-        return "I"*arg
+        return "I" * arg
     elif arg <= 5:
-        return "I"*(5-arg)+"V"
+        return "I" * (5 - arg) + "V"
     elif arg <= 8:
-        return "V"+"I"*(arg-5)
-    else:
-        return "I"*(10-arg)+"X"
+        return "V" + "I" * (arg - 5)
+    elif arg <= 10:
+        return "I" * (10 - arg) + "X"
+    elif arg == 27:
+        return "X"*(27//10)+"V"+"I"*(arg-25)
