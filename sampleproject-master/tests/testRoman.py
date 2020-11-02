8063f73c6d5a66e0a64d7295c88aa457815ecb0d
Katarzyna Winkler <kasia.winkler99@gmail.com>
2020-11-02 20:56:24 +0100

    02 - test_2_is_two_i_s

--- sampleproject-master/tests/testRoman.py
+++ sampleproject-master/tests/testRoman.py
@@ -6,10 +6,10 @@ class RomanNumeralsTest(unittest.TestCase):
     def test_1_is_a_single_i(self):
         print(roman)
         self.assertEqual(roman(1), "I")
-    #
-    # def test_2_is_two_i_s(self):
-    #     self.assertEqual(roman(2), "II")
-    #
+
+    def test_2_is_two_i_s(self):
+        self.assertEqual(roman(2), "II")
+
     # def test_3_is_three_i_s(self):
     #     self.assertEqual(roman(3), "III")
     #
