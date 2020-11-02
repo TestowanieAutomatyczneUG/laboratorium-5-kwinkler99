e2249c68853f1cd12e950f88c1063f4e70c58ca9
Katarzyna Winkler <kasia.winkler99@gmail.com>
2020-11-02 22:15:21 +0100

    02 - test_3000_is_three_m_s program

--- sampleproject-master/src/roman/roman.py
+++ sampleproject-master/src/roman/roman.py
@@ -13,10 +13,10 @@ def roman(arg):
         ]
     rom_val = ''
     i = 0
-    if arg < 1500:
-        while arg > 0:
-            for _ in range(arg // int_val[i]):
-                rom_val += rom[i]
-                arg -= int_val[i]
-            i += 1
-        return rom_val
+
+    while arg > 0:
+        for _ in range(arg // int_val[i]):
+            rom_val += rom[i]
+            arg -= int_val[i]
+        i += 1
+    return rom_val
