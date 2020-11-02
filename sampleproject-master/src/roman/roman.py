f2714e6c6f169f9e16d3c3af6ebc4a5bc6ae1fe2
Katarzyna Winkler <kasia.winkler99@gmail.com>
2020-11-02 22:14:42 +0100

    02 - test_1000_is_a_single_m program

--- sampleproject-master/src/roman/roman.py
+++ sampleproject-master/src/roman/roman.py
@@ -1,22 +1,22 @@
 def roman(arg):
     int_val = [
-            900, 500, 400,
+            1000, 900, 500, 400,
             100, 90, 50, 40,
             10, 9, 5, 4,
             1
             ]
     rom = [
-        "CM", "D", "CD",
+        "M", "CM", "D", "CD",
         "C", "XC", "L", "XL",
         "X", "IX", "V", "IV",
         "I"
         ]
     rom_val = ''
     i = 0
-
-    while arg > 0:
-        for _ in range(arg // int_val[i]):
-            rom_val += rom[i]
-            arg -= int_val[i]
-        i += 1
-    return rom_val
+    if arg < 1500:
+        while arg > 0:
+            for _ in range(arg // int_val[i]):
+                rom_val += rom[i]
+                arg -= int_val[i]
+            i += 1
+        return rom_val
